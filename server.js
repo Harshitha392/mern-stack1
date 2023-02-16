68ccf83a1f3c3b9ce4c821e948c17caddd3e9f03
Harshitha392 <harshitha.majety@gmail.com>
2023-02-16 16:46:36 +0530

    updated

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.get("/resume", function(req, res){
 	//res.send("I am Harshitha Majety");
 	res.sendFile(__dirname+"/resume.html");
 });
-app.get("/resume1", function(req, res){
-	//res.send("I am Harshitha Majety");
-	res.sendFile(__dirname+"/resume1.html");
-});
 mongoose.set('strictQuery',true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
 	if(err)
